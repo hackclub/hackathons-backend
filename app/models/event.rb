715573a6cebabd78c70fa9fecd4039fef570fad1
northeastprince <matt7@hey.com>
2023-07-13 18:54:10 -0400

    Add reasoning to Event#description

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
 
   def description
     if creator && !target
-      "#{action.humanize(capitalize: false)} by #{creator.name}"
+      "#{action.humanize(capitalize: false)} by #{creator.name}" # e.g. (hackathon) "approved by Matt" vs "Matt removed X" ∨∨∨
     else
       [creator.name, action.humanize(capitalize: false), target.name].compact.join(" ")
     end
