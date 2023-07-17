74b2e2b44737523af77927daf90712fe33d3ed04
northeastprince <matt7@hey.com>
2023-07-17 07:31:13 -0400

    Make Hackathon dates `NOT NULL`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_134548) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_112942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_134548) do
   create_table "hackathons", force: :cascade do |t|
     t.string "name", null: false
     t.integer "status", default: 0, null: false
-    t.datetime "starts_at"
-    t.datetime "ends_at"
+    t.datetime "starts_at", null: false
+    t.datetime "ends_at", null: false
     t.string "country_code"
     t.string "province"
     t.string "city"
