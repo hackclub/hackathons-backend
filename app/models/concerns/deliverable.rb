f60fd3afa54e801d9506c5214465f60fd9f1785d
northeastprince <matt7@hey.com>
2023-07-21 10:49:11 -0400

    Revert Deliverable modifications
    will tackle later

--- app/models/concerns/deliverable.rb
+++ app/models/concerns/deliverable.rb
@@ -7,19 +7,11 @@ module Deliverable
   end
 
   def deliver_later
-    job = delivery.deliver_later
-
-    if respond_to?(:record)
-      record(:delivery_enqueued, sidekiq_job_id: job.provider_job_id)
-    end
+    delivery.deliver_later
   end
 
   def deliver_now
     delivery.deliver_now
-
-    if respond_to?(:record)
-      record(:sent, recipient_email_address: delivery.to)
-    end
   end
 
   def delivery
