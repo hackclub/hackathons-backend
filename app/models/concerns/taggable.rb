979333e84e92cc23ebbab110bb06baf937246bac
northeastprince <matt7@hey.com>
2023-07-22 11:30:32 -0400

    🧹

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -6,35 +6,31 @@ module Taggable
     has_many :tags, through: :taggings
   end
 
+  def tagged_with?(tags_or_names)
+    Array(tags_or_names).all? do |tag|
+      tag = Tag.find_by(name: tag) unless tag.is_a? Tag
+      tags.include? tag
+    end
+  end
+
   def tag_with(tags_or_names)
-    Array(tags_or_names).each do |tag_or_name|
-      if tag_or_name.is_a? Tag
-        taggings.create_or_find_by! tag: tag_or_name
-      else
-        tag = Tag.find_by name: tag_or_name
-        taggings.create_or_find_by!(tag:) if tag
-      end
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_by(name: tag) unless tag.is_a? Tag
+      taggings.create_or_find_by tag: tag
     end
   end
 
   def tag_with!(tags_or_names)
-    Array(tags_or_names).each do |tag_or_name|
-      if tag_or_name.is_a? Tag
-        tag_with tag_or_name
-      else
-        tag_with Tag.find_or_create_by!(name: tag_or_name)
-      end
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_or_initialize_by(name: tag) unless tag.is_a? Tag
+      taggings.create_or_find_by tag: tag
     end
   end
 
   def untag(tags_or_names)
-    Array(tags_or_names).each do |tag_or_name|
-      if tag_or_name.is_a? Tag
-        taggings.destroy_by tag: tag_or_name
-      else
-        tag = Tag.find_by name: tag_or_name
-        taggings.destroy_by tag: tag
-      end
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_by name: tag unless tag.is_a? Tag
+      taggings.destroy_by tag: tag
     end
   end
 end
