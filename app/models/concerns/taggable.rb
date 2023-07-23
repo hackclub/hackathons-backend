f417e084eda649fee51e738d59bbd0e222ddc1aa
Matt Almeida <55164724+northeastprince@users.noreply.github.com>
2023-07-23 09:24:09 -0400

    Refactor taggings (#58)

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -4,34 +4,41 @@ module Taggable
   included do
     has_many :taggings, as: :taggable, dependent: :destroy
     has_many :tags, through: :taggings
+
+    scope :tagged_with, ->(tags_or_names) {
+      tags, names = Array(tags_or_names).partition { |tag| tag.is_a? Tag }
+      tags.concat Tag.where(name: names)
+
+      joins(:taggings).where(taggings: {tag: tags})
+    }
   end
 
-  def tag_as(name)
-    if (tag = Tag.find_by(name: name))
-      tag_with tag
+  def tagged_with?(tags_or_names)
+    return false unless tags_or_names.present?
+    Array(tags_or_names).all? do |tag|
+      tag = Tag.find_by(name: tag) unless tag.is_a? Tag
+      tags.include? tag
     end
   end
 
-  def tag_as!(name)
-    tag = Tag.find_or_create_by! name: name
-    tag_with tag
-  end
-
-  def tag_with(tags)
-    Array(tags).each do |tag|
-      taggings.create_or_find_by! tag:
+  def tag_with(tags_or_names)
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_by(name: tag) unless tag.is_a? Tag
+      taggings.create_or_find_by tag: tag
     end
   end
 
-  def untag_as(name)
-    if (tag = Tag.find_by(name:))
-      untag tag
+  def tag_with!(tags_or_names)
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_or_initialize_by(name: tag) unless tag.is_a? Tag
+      taggings.create_or_find_by tag: tag
     end
   end
 
-  def untag(tags)
-    Array(tags).each do |tag|
-      taggings.destroy_by tag:
+  def untag(tags_or_names)
+    Array(tags_or_names).each do |tag|
+      tag = Tag.find_by name: tag unless tag.is_a? Tag
+      taggings.destroy_by tag: tag
     end
   end
 end
--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -1,73 +1,92 @@
 require "test_helper"
 
 class TaggingTest < ActiveSupport::TestCase
-  test "tagging a hackathon by name" do
-    hackathon = hackathons(:assemble)
+  setup do
+    @hackathon = hackathons(:assemble)
+  end
 
-    assert_no_difference -> { hackathon.tags.count } do
-      hackathon.tag_as "Ian Approved"
+  test "checking for tags" do
+    assert @hackathon.tags.none?
+
+    @hackathon.taggings.create! tag: Tag.new(name: "Tag")
+
+    assert @hackathon.tagged_with? "Tag"
+    assert @hackathon.tagged_with? Tag.last
+
+    assert_not @hackathon.tagged_with? "Nonexistent"
+    assert_not @hackathon.tagged_with? nil
+  end
+
+  test "scoping by tags" do
+    assert Hackathon.tagged_with("Great Snacks").none?
+    assert Hackathon.tagged_with(Tag.last).none?
+
+    @hackathon.taggings.create! tag: Tag.new(name: "Great Snacks")
+
+    assert_includes Hackathon.tagged_with("Great Snacks"), @hackathon
+
+    Tag.create! name: "Matt A. Approved"
+    assert_not_includes Hackathon.tagged_with("Matt A. Approved"), @hackathon
+    assert_not_includes Hackathon.tagged_with(Tag.last), @hackathon
+  end
+
+  test "tagging a hackathon by name" do
+    assert_no_difference -> { @hackathon.tags.count } do
+      @hackathon.tag_with "Ian Approved"
     end
 
-    assert_difference -> { hackathon.tags.count }, 1 do
-      hackathon.tag_as! "Matt A. Approved"
+    assert_difference -> { @hackathon.tags.count }, 1 do
+      @hackathon.tag_with! "Matt A. Approved"
     end
   end
 
   test "tagging a hackathon" do
-    hackathon = hackathons(:assemble)
-
-    assert_no_difference -> { hackathon.tags.count } do
-      hackathon.tag_with Tag.last
+    assert_no_difference -> { @hackathon.tags.count } do
+      @hackathon.tag_with Tag.last
     end
 
     Tag.create! name: "Matt A. Approved"
 
-    assert_difference -> { hackathon.tags.count }, 1 do
-      hackathon.tag_with Tag.last
+    assert_difference -> { @hackathon.tags.count }, 1 do
+      @hackathon.tag_with Tag.last
     end
   end
 
   test "tagging a hackathon with EVERYTHING" do
-    hackathon = hackathons(:assemble)
-
-    assert_no_difference -> { hackathon.tags.count } do
-      hackathon.tag_with Tag.all
+    assert_no_difference -> { @hackathon.tags.count } do
+      @hackathon.tag_with Tag.all
     end
 
     Tag.create! name: "Matt A. Approved"
     Tag.create! name: "Best Hackathon Ever"
     Tag.create! name: "Great Snacks"
 
-    assert_difference -> { hackathon.tags.count }, 3 do
-      hackathon.tag_with Tag.all
+    assert_difference -> { @hackathon.tags.count }, 3 do
+      @hackathon.tag_with Tag.all
     end
   end
 
   test "untagging a hackathon by name" do
-    hackathon = hackathons(:assemble)
-
-    assert_no_difference -> { hackathon.tags.count } do
-      hackathon.untag_as "Great Snacks"
+    assert_no_difference -> { @hackathon.tags.count } do
+      @hackathon.untag "Great Snacks"
     end
 
-    hackathon.tag_as! "Great Snacks"
+    @hackathon.tag_with! "Great Snacks"
 
-    assert_difference -> { hackathon.tags.count }, -1 do
-      hackathon.untag_as "Great Snacks"
+    assert_difference -> { @hackathon.tags.count }, -1 do
+      @hackathon.untag "Great Snacks"
     end
   end
 
   test "untagging a hackathon" do
-    hackathon = hackathons(:assemble)
-
-    assert_no_difference -> { hackathon.tags.count } do
-      hackathon.untag Tag.last
+    assert_no_difference -> { @hackathon.tags.count } do
+      @hackathon.untag Tag.last
     end
 
-    hackathon.tag_as! "Great Snacks"
+    @hackathon.tag_with! "Great Snacks"
 
-    assert_difference -> { hackathon.tags.count }, -1 do
-      hackathon.untag Tag.last
+    assert_difference -> { @hackathon.tags.count }, -1 do
+      @hackathon.untag Tag.last
     end
   end
 end
