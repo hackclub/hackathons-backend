c3318be2cf9fd43ea27189875e871a586c5002b7
northeastprince <matt7@hey.com>
2023-07-25 22:02:42 -0400

    Refactor authentication / mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,15 @@
 class UserMailer < ApplicationMailer
-  def authentication
-    @authentication = params[:authentication]
-    @user = @authentication.user
+  before_action :set_user
 
-    mail to: @user.email_address, subject: "Sign in to Hackathons"
+  def authentication(authentication = nil)
+    @authentication = authentication || @user.authentications.last
+
+    mail to: @authentication.user.email_address, subject: "Sign in to Hackathons"
+  end
+
+  private
+
+  def set_user
+    @user = params & [:user] || Current.user
   end
 end
