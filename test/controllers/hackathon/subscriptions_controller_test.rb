fbab66347327875bb098f224a15e74d6984545ae
Gary Tou <gary@garytou.com>
2023-07-25 23:14:53 -0700

    DRY up controller test

--- test/controllers/hackathon/subscriptions_controller_test.rb
+++ test/controllers/hackathon/subscriptions_controller_test.rb
@@ -1,20 +1,20 @@
 require "test_helper"
 
 class Hackathon::SubscriptionsControllerTest < ActionDispatch::IntegrationTest
-  test "should unsubscribe" do
-    subscription = hackathon_subscriptions(:gary_seattle)
+  setup do
+    @subscription = hackathon_subscriptions(:gary_seattle)
+  end
 
-    get subscription.unsubscribe_url
+  test "should unsubscribe" do
+    get @subscription.unsubscribe_url
     assert_response :success
 
     assert_match "You have unsubscribed", @response.body
-    assert_match subscription.location, @response.body
+    assert_match @subscription.location, @response.body
   end
 
   test "should expire" do
-    subscription = hackathon_subscriptions(:gary_seattle)
-
-    unsub_url = subscription.unsubscribe_url
+    unsub_url = @subscription.unsubscribe_url
     expiration_at = Hackathon::Subscription::Status::UNSUBSCRIBE_EXPIRATION.from_now + 1.second
 
     travel_to expiration_at do
@@ -26,9 +26,7 @@ class Hackathon::SubscriptionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should handle invalid signature" do
-    subscription = hackathon_subscriptions(:gary_seattle)
-
-    get unsubscribe_subscription_url(subscription) # no signature
+    get unsubscribe_subscription_url(@subscription) # no signature
     assert_response :success
 
     assert_match "expired", @response.body
