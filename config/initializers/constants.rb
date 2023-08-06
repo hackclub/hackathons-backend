4f4aaab13c50e32030f0e293015ec87e72f53317
Gary Tou <gary@garytou.com>
2023-08-06 16:46:24 -0700

    Extract Hack Club address into global constant

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,12 @@
 module Hackathons
   WEBSITE = "https://hackathons.hackclub.com"
   SUPPORT_EMAIL = "bank@hackclub.com"
+  HACK_CLUB_ADDRESS = {
+    full: "8605 Santa Monica Blvd #86294 West Hollywood, CA 90069",
+
+    street: "8605 Santa Monica Blvd #86294 #86294",
+    city: "West Hollywood",
+    province: "CA",
+    zip: "90069"
+  }
 end
