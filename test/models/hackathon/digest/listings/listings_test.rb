a41a6186c79b917b65eebd3226ccd0b1be30d6ae
Gary Tou <gary@garytou.com>
2023-08-07 17:57:49 -0700

    Refactor test "create a digest with no matching listings"
    Co-authored-by: Matt Almeida <matt7@hey.com>

--- test/models/hackathon/digest/listings/listings_test.rb
+++ test/models/hackathon/digest/listings/listings_test.rb
@@ -1,10 +1,9 @@
 require "test_helper"
 
 class Hackathon::Digest::ListingsTest < ActiveSupport::TestCase
-  test "creating a digest for user without subscriptions" do
+  test "create a digest with no matching listings" do
     user = User.create!(email_address: "test@example.com")
-    assert_raises ActiveRecord::RecordInvalid, "Should raise error due to listings being empty" do
-      Hackathon::Digest.create! recipient: user
-    end
+    hackathon = Hackathon::Digest.new recipient: user
+    assert_not hackathon.save
   end
 end
