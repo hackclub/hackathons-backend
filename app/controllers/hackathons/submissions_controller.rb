b6058f3bc76c3c0f0da1e73cd887ee078e369234
northeastprince <matt7@hey.com>
2023-08-09 17:52:19 -0400

    Hackathon request -> submission

--- app/controllers/hackathons/submissions_controller.rb
+++ app/controllers/hackathons/submissions_controller.rb
@@ -2,7 +2,7 @@ class Hackathons::SubmissionsController < ApplicationController
   def index
     @hackathons = Hackathon.not_approved.where applicant: Current.user
 
-    redirect_to new_hackathons_request_path if @hackathons.none?
+    redirect_to new_hackathons_submission_path if @hackathons.none?
   end
 
   def new
@@ -12,7 +12,7 @@ class Hackathons::SubmissionsController < ApplicationController
   def create
     @hackathon = Hackathon.new(hackathon_params)
     if @hackathon.save context: :submit
-      redirect_to hackathons_requests_path, notice: "Your hackathon has been submitted for approval!"
+      redirect_to hackathons_submissions_path, notice: "Your hackathon has been submitted for approval!"
     else
       render :new, status: :unprocessable_entity
     end
