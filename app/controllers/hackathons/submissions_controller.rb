7cf89b74efdeedea7408f910a746a83d1dadc322
northeastprince <matt7@hey.com>
2023-08-09 20:01:42 -0400

    Ask if the hackathon offers financial assistance

--- app/controllers/hackathons/submissions_controller.rb
+++ app/controllers/hackathons/submissions_controller.rb
@@ -11,8 +11,12 @@ class Hackathons::SubmissionsController < ApplicationController
   end
 
   def create
-    @hackathon = Hackathon.new(hackathon_params)
+    @hackathon = Hackathon.new(hackathon_params.except(:offers_financial_assistance))
     if @hackathon.save context: :submit
+      if hackathon_params[:offers_financial_assistance] == "true"
+        @hackathon.tag_with! "Offers Financial Assistance"
+      end
+
       redirect_to hackathons_submissions_path, notice: "Your hackathon has been submitted for approval!"
     else
       render :new, status: :unprocessable_entity
@@ -35,6 +39,7 @@ class Hackathons::SubmissionsController < ApplicationController
       :ends_at,
       :address,
       :expected_attendees,
+      :offers_financial_assistance,
       swag_mailing_address: [
         :line1,
         :line2,
