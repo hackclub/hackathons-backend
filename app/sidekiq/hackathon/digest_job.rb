24c050cac514dfc7e4b3076bd95d2f795afe2722
Gary Tou <gary@garytou.com>
2023-08-11 22:19:48 -0700

    Send admin summary after creating digests (#98)

--- app/sidekiq/hackathon/digest_job.rb
+++ app/sidekiq/hackathon/digest_job.rb
@@ -3,6 +3,7 @@ class Hackathon::DigestJob
   sidekiq_options retry: 20 # up to 6.5 days
 
   def perform
+    digests = []
     users.find_each do |user|
       # Skip if the user has a digest created in the last 6 days
       next if user.digests.last&.created_at&.after? 6.days.ago
@@ -11,7 +12,10 @@ class Hackathon::DigestJob
 
       # Save unless there are no listings to send
       digest.save! unless digest.invalid? && digest.errors.include?(:listings)
+      digests << digest if digest.persisted?
     end
+  ensure
+    Hackathons::DigestMailer.with(digests:).admin_summary.deliver_later if digests.any?
   end
 
   private
