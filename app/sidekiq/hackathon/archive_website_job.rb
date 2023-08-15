f325e0cbcef0335e0179d094e25dcbcfbee49200
Sam Poder <sam.r.poder@gmail.com>
2023-08-14 19:36:25 -0600

    Use `&&` and `||`

--- app/sidekiq/hackathon/archive_website_job.rb
+++ app/sidekiq/hackathon/archive_website_job.rb
@@ -6,7 +6,7 @@ class Hackathon::ArchiveWebsiteJob
   def perform
     hackathons.find_each do |hackathon|
       response = Faraday.get(hackathon.website)
-      if response.status != 200 and response.body.include? hackathon.name
+      if response.status != 200 && response.body.include? hackathon.name
         Faraday.get("https://web.archive.org/save/" + hackathon.website.sub(/^https?:\/\/(www.)?/, ""))
       end
     end
