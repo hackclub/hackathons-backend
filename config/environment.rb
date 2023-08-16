37775224a752e06c84fa1077b0e12cff7256c564
Gary Tou <gary@garytou.com>
2023-08-16 02:17:28 -0700

    Send logs to AppSignal

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+# Use AppSignal's logger
+Rails.logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails"))
+
 # Initialize the Rails application.
 Rails.application.initialize!
