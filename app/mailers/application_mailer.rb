5899603173294e0dd001d853fb9a859cbb9ea1ee
Gary Tou <gary@garytou.com>
2023-08-16 18:37:49 -0700

    Remove default unsubscribe links (#135)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,7 +4,6 @@ class ApplicationMailer < ActionMailer::Base
 
   helper :mailer, :application
 
-  before_action :set_default_unsubscribe_urls
   after_action :set_unsubscribe_header
 
   private
@@ -12,11 +11,6 @@ class ApplicationMailer < ActionMailer::Base
   def set_unsubscribe_urls_for(user)
     @unsubscribe_url = Hackathon::Subscription.unsubscribe_all_url_for user
     @email_preferences_url = Hackathon::Subscription.manage_subscriptions_url_for user
-  end
-
-  def set_default_unsubscribe_urls
-    @unsubscribe_url = root_url
-    @email_preferences_url = root_url
   end
 
   def set_unsubscribe_header
