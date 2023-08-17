ac4a764c28dddbd1995af13c6e39c8b4e2660a9e
northeastprince <matt7@hey.com>
2023-08-17 07:17:58 +0200

    `standardrb -a`

--- app/controllers/admin/hackathons_controller.rb
+++ app/controllers/admin/hackathons_controller.rb
@@ -20,7 +20,7 @@ class Admin::HackathonsController < Admin::BaseController
       respond_to do |format|
         format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash") }
 
-        #format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
