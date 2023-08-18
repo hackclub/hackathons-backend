218b7a42e4809c887f496224a794063ca1f30e3d
Gary Tou <gary@garytou.com>
2023-08-18 12:29:56 -0700

    AppSignal JavaScript: Don't monitor development + Install plugins (#149)
    * Move `appsignal` outside of `javascript/controllers`
    
    * Install AppSignal plugins
    
    * Hack to prevent development errors from being logged

---
+++ app/javascript/appsignal.js
@@ -0,0 +1,14 @@
+import Appsignal from "@appsignal/javascript";
+import {plugin as breadcrumbsConsole} from "@appsignal/plugin-breadcrumbs-console";
+import {plugin as breadcrumbsNetwork} from "@appsignal/plugin-breadcrumbs-network";
+import {plugin as pathDecorator} from "@appsignal/plugin-path-decorator"
+
+export const appsignal = new Appsignal({
+  key: Hackathons.appsignal.key,
+});
+
+appsignal.use(breadcrumbsConsole());
+appsignal.use(breadcrumbsNetwork());
+appsignal.use(pathDecorator());
+
+console.log("Appsignal:", appsignal);
