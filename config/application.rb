78845b2912101bbcf01150749f95e8308299bab1
northeastprince <matt7@hey.com>
2023-08-18 21:49:22 +0200

    Remove redundant queue name overrides

--- config/application.rb
+++ config/application.rb
@@ -12,15 +12,6 @@ module Hackathons
 
     config.active_record.encryption.encrypt_fixtures = true
 
-    # Don't use separate queues for jobs enqueued by Rails:
-    config.action_mailer.deliver_later_queue_name = nil
-    config.action_mailbox.queues.routing = nil
-    config.action_mailbox.queues.incineration = nil
-    config.active_storage.queues.analysis = nil
-    config.active_storage.queues.transform = nil
-    config.active_storage.queues.mirror = nil
-    config.active_storage.queues.purge = nil
-
     host = ENV["HOST"] || "localhost:3000"
     Rails.application.routes.default_url_options[:host] = host
     config.action_mailer.default_url_options = {host:}
