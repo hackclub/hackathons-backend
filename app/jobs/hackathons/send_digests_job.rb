7e803acf44dcfe43982d3f7f11448f18e4db9226
Sam Poder <sam.r.poder@gmail.com>
2023-08-22 22:40:20 +0000

    Standard

--- app/jobs/hackathons/send_digests_job.rb
+++ app/jobs/hackathons/send_digests_job.rb
@@ -11,7 +11,6 @@ class Hackathons::SendDigestsJob < ApplicationJob
       digest.save! unless digest.invalid? && digest.listings.none?
       sent_digests << digest if digest.persisted?
     end
-    
   ensure
     Hackathons::SendOrganizerSummariesJob(sent_digests).perform if sent_digests.any?
     Hackathons::DigestMailer.admin_summary(sent_digests).deliver_later if sent_digests.any?
