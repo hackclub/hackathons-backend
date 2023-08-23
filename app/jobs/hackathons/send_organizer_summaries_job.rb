8c594d4dd692c92c806f499925409e217ab70491
Sam Poder <sam.r.poder@gmail.com>
2023-08-22 23:13:19 -0700

    Update send_organizer_summaries_job.rb

--- app/jobs/hackathons/send_organizer_summaries_job.rb
+++ app/jobs/hackathons/send_organizer_summaries_job.rb
@@ -13,7 +13,7 @@ class Hackathons::SendOrganizerSummariesJob < ApplicationJob
     @listed_hackathons = @sent_digests_by_hackathons.keys
     @listed_hackathons.each do |hackathon|
       sent_digests = @digests_by_hackathons[hackathon]
-      Hackathons::DigestMailer.admin_summary(sent_digests, hackathon).deliver_later if @sent_digests.any?
+      Hackathons::DigestMailer.organizer_summary(sent_digests, hackathon).deliver_later if @sent_digests.any?
     end
   end
 end
