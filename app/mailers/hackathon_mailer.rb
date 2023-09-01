b5020859b2b18d7ff9522e41ea922da11830cd74
northeastprince <matt7@hey.com>
2023-09-01 14:08:28 -0400

    Set hackathon param before every action in HackathonMailer

--- app/mailers/hackathon_mailer.rb
+++ app/mailers/hackathon_mailer.rb
@@ -1,7 +1,14 @@
 class HackathonMailer < ApplicationMailer
+  before_action :set_hackathon
+
   def submission
-    @hackathon = params[:hackathon]
     email_addresses = User.admins.collect(&:email_address)
     mail to: email_addresses, subject: 'A new Hackathon entry was submitted!'
+  end
+
+  private
+
+  def set_hackathon
+    @hackathon = params[:hackathon]
   end
 end
