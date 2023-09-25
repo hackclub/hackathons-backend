6d17bf32cc041d8e41f543da3e04690266270257
Matt Almeida <matt7@hey.com>
2023-09-25 12:11:03 -0400

    Don't parallelize system tests (#244)
    Hopefully this will solve the extreme flakiness I've been seeing in GitHub Actions...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require "sidekiq/testing"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: ENV.fetch("PARALLEL_WORKERS", :number_of_processors))
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
