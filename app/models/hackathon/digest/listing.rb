81e81a649bf45cdceba7d3a26f7b334287d0cdd2
Sam Poder <sam.r.poder@gmail.com>
2023-09-26 16:02:06 -0700

    Update listing.rb

--- app/models/hackathon/digest/listing.rb
+++ app/models/hackathon/digest/listing.rb
@@ -2,7 +2,7 @@ class Hackathon::Digest::Listing < ApplicationRecord
   belongs_to :digest, class_name: "Hackathon::Digest"
   belongs_to :hackathon
   belongs_to :subscription
-  
+
   def previously_listed_for_recipient?(recipient: digest.recipient)
     Hackathon::Digest::Listing.where(hackathon: hackathon, recipient: recipient) > 1
   end
