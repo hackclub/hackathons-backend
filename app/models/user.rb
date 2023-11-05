b71832c8a030aec965e364f2d279cb0f670195ea
Matt Almeida <matt7@hey.com>
2023-11-05 15:55:31 -0500

    Don't redundantly specify `foreign_key`s (#287)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   include Named
   include Privileged # depends on Eventable
 
-  has_many :subscriptions, class_name: "Hackathon::Subscription", foreign_key: "subscriber_id", inverse_of: :subscriber, dependent: :destroy
-  has_many :digests, class_name: "Hackathon::Digest", foreign_key: "recipient_id", inverse_of: :recipient, dependent: :destroy
+  has_many :subscriptions, class_name: "Hackathon::Subscription", inverse_of: :subscriber, dependent: :destroy
+  has_many :digests, class_name: "Hackathon::Digest", inverse_of: :recipient, dependent: :destroy
 end
