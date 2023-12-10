9b9421a85ddaebf5cc1ff3658fc0a34fd63f0861
matt7@hey.com <matt7@hey.com>
2023-12-10 15:35:49 -0500

    💀

--- app/models/event/request.rb
+++ app/models/event/request.rb
@@ -8,8 +8,8 @@ class Event::Request < ApplicationRecord
   private
 
   def set_from_current
-    self.uuid = Current.request_id,
-    self.user_agent = Current.user_agent,
+    self.uuid = Current.request_id
+    self.user_agent = Current.user_agent
     self.ip_address = Current.ip_address
   end
 end
