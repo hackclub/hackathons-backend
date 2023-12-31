4abf6eda7a3bcc735f6c328140e22a1b636602b1
northeastprince <matt7@hey.com>
2023-12-30 22:16:45 -0500

    Fix scope to handle setting not being set

--- app/models/user/settings.rb
+++ app/models/user/settings.rb
@@ -10,7 +10,7 @@ module User::Settings
   extend ActiveSupport::Concern
 
   included do
-    scope :with_setting_not_disabled, ->(setting) { where("settings ->> ? != 'false'", setting.to_s) }
+    scope :with_setting_not_disabled, ->(setting) { where("settings ->> ? IS NULL OR settings ->> ? != 'false'", setting.to_s, setting.to_s) }
   end
 
   User::SETTINGS.each do |setting|
