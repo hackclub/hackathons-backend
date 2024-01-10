1befb76a137a2ab880ecf756b63ac91cd3761ce2
Matt Almeida <matt7@hey.com>
2024-01-10 10:51:56 -0500

    Add public database dumps of the hackathons table (#393)

---
+++ app/controllers/database_dumps_controller.rb
@@ -0,0 +1,38 @@
+class DatabaseDumpsController < ApplicationController
+  before_action :set_database_dump, except: [:index, :create]
+
+  def index
+    @database_dumps = DatabaseDump.all
+  end
+
+  def create
+    @database_dump = DatabaseDump.create! if Current.user&.admin?
+
+    redirect_to database_dumps_path
+  end
+
+  def edit
+  end
+
+  def update
+    @database_dump.update!(database_dump_params) if Current.user&.admin?
+
+    redirect_to database_dumps_path
+  end
+
+  def destroy
+    @database_dump.destroy! if Current.user&.admin?
+
+    redirect_to database_dumps_path
+  end
+
+  private
+
+  def set_database_dump
+    @database_dump = DatabaseDump.find(params[:id])
+  end
+
+  def database_dump_params
+    params.require(:database_dump).permit(:name)
+  end
+end
