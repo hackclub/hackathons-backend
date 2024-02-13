0b0a1d28383e5c140329bc151d0b239fd574d947
Matt Almeida <matt7@hey.com>
2024-02-13 18:53:18 -0500

    `compact_blank`
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/models/mailing_address.rb
+++ app/models/mailing_address.rb
@@ -6,7 +6,7 @@ class MailingAddress < ApplicationRecord
 
   def to_s
     components = []
-    components << [line1, line2].reject(&:blank?).compact.join(" ")
+    components << [line1, line2].compact_blank.join(" ")
     components << city << province << postal_code << country_code
 
     components.compact_blank.join(", ")
