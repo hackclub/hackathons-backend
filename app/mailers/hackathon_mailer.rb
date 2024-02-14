984677be8190b2bf4bc474b7b84184f32f45b319
Matt Almeida <matt7@hey.com>
2024-02-13 19:16:09 -0500

    Make it actually work

--- app/mailers/hackathon_mailer.rb
+++ app/mailers/hackathon_mailer.rb
@@ -2,6 +2,7 @@ class HackathonMailer < ApplicationMailer
   before_action :set_hackathon
 
   def swag_request
+    @mailing_address = @hackathon.swag_request.mailing_address
     mail(to: Rails.application.credentials.swag_requests_email_address || "swag@hackathons.test")
   end
 
