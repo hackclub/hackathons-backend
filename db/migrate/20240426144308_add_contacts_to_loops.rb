75f428e727390d64943848cc6a2d1c965410714b
Matt Almeida <matt7@hey.com>
2024-04-27 12:44:35 -0400

    Throttle initial backfill to avoid rate limit

--- db/migrate/20240426144308_add_contacts_to_loops.rb
+++ db/migrate/20240426144308_add_contacts_to_loops.rb
@@ -2,8 +2,8 @@ class AddContactsToLoops < ActiveRecord::Migration[7.2]
   def change
     return unless Rails.env.production?
 
-    User.includes(:subscriptions).where(subscriptions: {status: :active}).find_each do |user|
-      LoopsSynchronizationJob.perform_later(user)
+    User.includes(:subscriptions).where(subscriptions: {status: :active}).find_each.with_index do |user, index|
+      LoopsSynchronizationJob.set(wait: 3.seconds * index).perform_later(user)
     end
   end
 end
