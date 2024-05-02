bb6c7851bdebbfd9a819c57ff328c63b5675485a
northeastprince <matt7@hey.com>
2024-05-02 13:30:39 -0400

    Remove unused test files

--- test/models/database_dump_test.rb
+++ test/models/database_dump_test.rb
@@ -2,8 +2,7 @@ require "test_helper"
 
 class DatabaseDumpTest < ActiveSupport::TestCase
   test "dumping" do
-    dump = DatabaseDump.new
-    dump.process
+    dump = DatabaseDump.create!
 
     assert dump.processed?
     assert dump.file.attached?
