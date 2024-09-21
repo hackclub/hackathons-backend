8459cacafeba8458df684f5ca6c50e86ce7b2408
Matt Almeida <matt7@hey.com>
2024-09-21 10:58:41 -0400

    Update hackathon_submission_test.rb

--- test/system/hackathon_submission_test.rb
+++ test/system/hackathon_submission_test.rb
@@ -48,7 +48,7 @@ class HackathonSubmissionTest < ApplicationSystemTestCase
 
     click_on "Submit for Review"
     wait_for_stimulus
-    assert_text(/submitted/i, wait: 10.seconds)
+    assert_text(/submitted/i, wait: 12.seconds)
 
     assert_equal "not.a.user.yet@hey.test", Hackathon.last.applicant.email_address
     assert_equal "Assemble", Hackathon.last.name
