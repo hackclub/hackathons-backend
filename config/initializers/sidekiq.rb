d9eb534e76a0d1a0c36e181043e2cfac2ec99270
northeastprince <matt7@hey.com>
2024-10-04 12:52:45 -0400

    Limit mailer concurrency to avoid SES rate limits
    Closes #543

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,12 @@ Sidekiq.configure_server do |config|
     url: ENV["REDIS_URL"],
     ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
   }
+
+  config.capsule("AWS SES") do |capsule| # avoid rate limits
+    Rails.configuration.action_mailer.deliver_later_queue_name = "SES"
+    capsule.queue = "SES"
+    capsule.concurrency = 2
+  end
 end
 
 Sidekiq.configure_client do |config|
