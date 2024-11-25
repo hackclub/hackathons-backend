cac649fd894feeb0097e3d51f859e3432a0d6548
Matt Almeida <matt7@hey.com>
2024-11-25 13:28:01 -0500

    Update action_mailer_concurrency.rb

--- config/initializers/action_mailer_concurrency.rb
+++ config/initializers/action_mailer_concurrency.rb
@@ -1,4 +1,4 @@
-config.after_initialize do
+Rails.application.config.after_initialize do
   # AWS SES rate-limits to 14/second, so we'll set it to 10 to be safe
   ActionMailer::MailDeliveryJob.limits_concurrency key: "mail", to: 10 * 1.minute.to_i, duration: 1.minute
 end
