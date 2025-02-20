73b2f97202915adf6bce0799f9a51a4d8358725f
Matt Almeida <matt7@hey.com>
2025-02-20 09:39:01 -0500

    Fix migration

--- db/migrate/20250220140522_alpha3_country_codes_to_alpha2.rb
+++ db/migrate/20250220140522_alpha3_country_codes_to_alpha2.rb
@@ -1,11 +1,11 @@
 class Alpha3CountryCodesToAlpha2 < ActiveRecord::Migration[8.1]
   def up
     Hackathon.where("LENGTH(country_code) = 3").find_each do
-      it.update! country_code: ISO3166::Country.from_alpha3_to_alpha2(it.country_code)
+      it.update_attribute! :country_code, ISO3166::Country.from_alpha3_to_alpha2(it.country_code)
     end
 
     Hackathon::Subscription.where("LENGTH(country_code) = 3").find_each do
-      it.update! country_code: ISO3166::Country.from_alpha3_to_alpha2(it.country_code)
+      it.update_attribute! :country_code, ISO3166::Country.from_alpha3_to_alpha2(it.country_code)
     end
   end
 
