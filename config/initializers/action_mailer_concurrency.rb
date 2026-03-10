8ee1505eb0ef3ad3b2319f877938e75f7895a23a
Matt Almeida <mail@mattalmeida.net>
2026-03-09 22:14:00 -0400

    Rate limit emails without monkey patching (#819)

--- config/initializers/action_mailer_concurrency.rb
+++ config/initializers/action_mailer_concurrency.rb
@@ -1,5 +1,1 @@
-Rails.application.config.after_initialize do
-  # AWS SES rate-limits to 14/second, so we'll set it to 10 to be safe
-  ActionMailer::MailDeliveryJob.include(RateLimitable)
-    .rate_limit to: 10, within: 1.second
-end
+Rails.application.config.action_mailer.delivery_job = "RateLimitedMailDeliveryJob"
