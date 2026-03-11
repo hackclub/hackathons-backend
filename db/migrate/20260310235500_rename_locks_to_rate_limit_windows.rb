7e47f4748769ccc1449913b6b6ce3cd9b5248056
Matt Almeida <matt7@hey.com>
2026-03-10 22:46:51 -0400

    newline

--- db/migrate/20260310235500_rename_locks_to_rate_limit_windows.rb
+++ db/migrate/20260310235500_rename_locks_to_rate_limit_windows.rb
@@ -15,6 +15,7 @@ class RenameLocksToRateLimitWindows < ActiveRecord::Migration[8.2]
         SQL
       end
     end
+
     remove_index :rate_limit_windows, :key
     add_index :rate_limit_windows, :key, unique: true
   end
