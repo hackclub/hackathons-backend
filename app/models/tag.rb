093261ba50bf1b941c708eaf805c7d5c5f9f7666
Matt Almeida <mail@mattalmeida.net>
2026-03-12 19:36:17 -0400

    Make sure taggables are touched

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,13 +2,21 @@ class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :taggings, dependent: :destroy
-  has_many :taggables, through: :taggings
 
-  after_update :touch_taggings, if: :saved_changes?
+  after_update :touch_taggables, if: :saved_changes?
 
   private
 
-  def touch_taggings
-    taggings.in_batches.touch_all
+  def touch_taggables
+    taggings.in_batches do |taggings|
+      taggable_ids_by_type_in(taggings).each do |taggable_type, ids|
+        taggable_type.safe_constantize&.where(id: ids)&.touch_all
+      end
+    end
+  end
+
+  def taggable_ids_by_type_in(taggings)
+    taggings.pluck(:taggable_type, :taggable_id)
+      .group_by(&:shift).transform_values!(&:flatten)
   end
 end
