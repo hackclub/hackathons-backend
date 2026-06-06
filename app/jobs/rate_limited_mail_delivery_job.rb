52f25dd81a0442f76180f10a5604fe0804179abc
Matt Almeida <mail@mattalmeida.net>
2026-06-06 17:55:01 -0400

    Don't rate limit mail deliveries on our end
    Not worth the hassle of using such a complex system, especially since there's no penalty / temp-ban SES gives us for exceeding it unlike the Internet Archive. If we need to, we can add error-based retries and backoff later.

--- app/jobs/rate_limited_mail_delivery_job.rb
+++
@@ -1,5 +0,0 @@
-class RateLimitedMailDeliveryJob < ActionMailer::MailDeliveryJob
-  include RateLimitable
-
-  rate_limit to: 10, within: 1.second
-end
--- config/initializers/action_mailer_concurrency.rb
+++
@@ -1,1 +0,0 @@
-Rails.application.config.action_mailer.delivery_job = "RateLimitedMailDeliveryJob"
